f76cc7fbc8dc7189c16c913b80a8bb201dcabc6b
Thomas Mahler <43560300+mahlert@users.noreply.github.com>
2019-12-17 18:09:02 -0800

    Updated bookmarklet with functions.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,43 @@
-(function() {
-    var elts = document.getElementsByTagName('p');
-    for (var i = 0; i < elts.length; i++) {
-      elts[i].style['background-color'] = '#C0C';
+(function(){
+    var nodes = Array.from(document.body.childNodes);
+    var names = ["warren", "trump", "booker", "biden", "sanders", "bennet", "bloomberg", "buttigieg", "delaney", "gabbard", "klobuchar", "patrick", "steyer", "walsh", "weld", "williamson", "yang"];
+    iterateChildren(nodes, names);
+})();
+
+function iterateChildren(nodes, names){
+    for(let i=0; i<nodes.length; i++){
+        searchNode(nodes[i], names);
     }
-  })();
+}
+
+function searchNode(node, names){
+    if(node.childNodes.length != 0){
+        iterateChildren(Array.from(node.childNodes), names);
+    }
+    if(node.innerHTML != null){
+        let para = node;
+        console.log(node);
+        var text = para.innerHTML;
+
+        console.log(text);
+
+        var res = text.trim().split(" ");
+
+        for(let j = 0; j < res.length; j++){
+
+            for(let k = 0; k < names.length; k++){
+
+                if(names[k] == res[j].toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")){
+
+                    console.log("Presidential Candidate!");
+                    let exp =  new RegExp(res[j], "g");
+                    para.innerHTML = para.innerHTML.replace(exp, "<span style='background-color: yellow'>" + res[j] + "</span>");
+
+                }
+            }
+            var i = 0;
+        }   
+
+    }
+
+}
